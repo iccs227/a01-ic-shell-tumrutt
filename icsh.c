8f4c3b5e2c307c36878aec813af4121631648213
tumrutt <win.thammarat@gmail.com>
2025-05-15 21:30:44 +0700

    feat: script mode

--- icsh.c
+++ icsh.c
@@ -12,12 +12,49 @@
 #define MAX_CMD_BUFFER 255
 int exit_code = 0;
 int update_command = 1; // boolean determining whether to update command or not
+char buffer[MAX_CMD_BUFFER];
 char prev_command[MAX_CMD_BUFFER];
 
-int main() {
+void process_file(const char *file_location) {
+    const char *ext =  strrchr(file_location, '.');
+    if (ext == NULL || strcmp(ext, ".sh") != 0) {
+        printf("Unsupported file type.\n");
+        exit_code = 65; // input data format is incorrect or unexpected.
+        return;
+    }
+
+    FILE* file = fopen(file_location, "r");
+
+    if (file == NULL) {
+        printf("Failed to open %s.\n", file_location);
+        perror("");
+        exit_code = 67; // invalid or unknown destination address.
+        return;
+    }
+
+    int resume = 1;
+    while (resume) {
+        if (fgets(buffer, MAX_CMD_BUFFER, file) == NULL) {
+            printf("\nExit command not found. Terminating the program with exit code 0\n");
+            exit_code = 0;
+            break;
+        }
+        resume = process_command(buffer);
+    }
+
+    fclose(file);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc > 1) { // process the file(s) and exit
+        for (int i = 1; i < argc; ++i) {
+            process_file(argv[i]);
+        }
+        return exit_code;
+    }
+
     // welcome();
 
-    char buffer[MAX_CMD_BUFFER];
     int resume = 1;
     while (resume) {
         printf("icsh $ ");
